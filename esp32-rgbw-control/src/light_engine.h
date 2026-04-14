5e50e2e9b417eddecf4d63db865acdeb73d96962
jiumu12 <jiumu12@users.noreply.github.com>
2026-04-14 05:58:24 +0000

    feat: ESP32 RGBW 灯带控制程序
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- esp32-rgbw-control/src/light_engine.h
+++ esp32-rgbw-control/src/light_engine.h
@@ -9,5 +9,6 @@ void setEffect(LedMode mode, Color color, uint8_t speed);
 void setPower(bool on);
 EffectParams getCurrentStatus();
 void updateLight();
+Color hsvToRgbw(uint16_t h, uint8_t s, uint8_t v);
 
 #endif
